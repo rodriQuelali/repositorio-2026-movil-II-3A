96dd3f4641721b63cc2c96a25f60aa5ba89d2e01
rodriquelali <rodriquelali@gmail.com>
2026-02-25 21:04:42 -0400

    feat(ui): Base UI.

---
+++ MyProyectCars/app/src/main/java/com/example/myproyectcars/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.example.myproyectcars.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
