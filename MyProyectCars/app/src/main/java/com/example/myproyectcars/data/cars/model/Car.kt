cfdf37a40880dffac01a72aee91a512df6350142
rodriquelali <rodriquelali@gmail.com>
2026-05-05 13:09:44 -0400

    hello word in flutter.

---
+++ MyProyectCars/app/src/main/java/com/example/myproyectcars/data/cars/model/Car.kt
@@ -0,0 +1,10 @@
+package com.example.myproyectcars.data.cars.model
+
+data class Car(
+    val id: Int,
+    val brand: String,
+    val model: String,
+    val year: Int,
+    val color: String
+    //metods no ay ...
+)
