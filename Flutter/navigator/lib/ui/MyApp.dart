99ba322ff050c256aa36110582396b07e0990746
rodriquelali <rodriquelali@gmail.com>
2026-07-28 08:52:06 -0400

    Ejemplo de navegacion.

---
+++ Flutter/navigator/lib/ui/MyApp.dart
@@ -0,0 +1,19 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:navigator/ui/HomePage.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
